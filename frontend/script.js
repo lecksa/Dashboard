62120af1555e31361494689b075ba78c30966c7d
lecksa <dolabesipa@gmail.com>
2024-02-17 11:29:02 +0500

    added

--- frontend/script.js
+++ frontend/script.js
@@ -10,7 +10,11 @@ close.onclick = () => {
     dialog.close()
 }
 
-let form = document.forms
+let tablisa = document.querySelector('.tablisa')
+let plitka = document.querySelector('.plitka')
+
+let plit = document.querySelector('.plit')
+
 let new_add = document.querySelector('#first')
 let tbody = document.querySelector('tbody')
 let lists = []
@@ -24,6 +28,7 @@ let type_inp = document.querySelector('.type')
 new_add.onclick = () => {
 
     if (title_inp.value.trim() !== '' && description_inp.value.trim() !== '' && time_inp.value.trim() !== '' && date_inp.value.trim() !== '' && type_inp.value.trim() === "выполнено" || type_inp.value.trim() === "не выполнено" || type_inp.value.trim() === "в прогрессе") {
+        
         let inputs = {
             title: title_inp.value.trim(),
             description: description_inp.value.trim(),
@@ -33,8 +38,13 @@ new_add.onclick = () => {
         }
 
         lists.push(inputs)
-        console.log(lists);
-        reload(lists, tbody)
+
+        if(tablisa.classList.includes('active_p')){
+            reload(lists, tbody)
+        }else{
+            reload_plitka(lists, plit)
+        }
+
         
         title_inp.value = ""
         description_inp.value = ""
@@ -67,5 +77,38 @@ function reload(arr, place) {
 
         place.append(tr)
         tr.append(title, description, date, time, type)
+
+        if(type.innerHTML === "в прогрессе"){
+            type.classList.add('blue_td')
+        }else if(type.innerHTML === "не выполнено"){
+            type.classList.add('red_td')
+        }
+    }
+}
+
+function reload_plitka(arr, place) {
+    place.innerHTML = ""
+
+    for(let item of arr){
+        let box = document.createElement('div')
+        let h1 = document.createElement('h1')
+        let p = document.createElement('p')
+        let time_date = document.createElement('div')
+        let date = document.createElement('span')
+        let time = document.createElement('span')
+        let h3 = document.createElement('h3')
+
+        box.classList.add('box')
+        time_date.classList.add('time_date')
+
+        h1.innerHTML = item.title
+        p.innerHTML = item.description
+        date.innerHTML = item.date
+        time.innerHTML = item.time
+        h3.innerHTML = item.type
+
+        place.append(box)
+        box.append(h1, p, time_date, h3)
+        time_date.append(date, time)
     }
 }
